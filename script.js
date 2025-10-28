c09089b188642e710bbee760d7fe4b11a834f7f9
Anirudh <sharmaanirudh200520@gmail.com>
2025-10-28 11:30:32 +0530

    Remove old HTML/JS/CSS files - keeping only React project

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add scroll effect to navigation
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 100) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
